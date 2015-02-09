e6388a05af7aaa160862f9f7df4e35832941f973
John Fulton <jf.1987@gmail.com>
2015-02-09 22:12:09 +1100

    The -w flag now limits the search to only events taking place in the next week

--- lib/meetup_thingy/event_finder.rb
+++ lib/meetup_thingy/event_finder.rb
@@ -1,10 +1,10 @@
 module MeetupThingy
   # class def
   class EventFinder
-    def extract_events(group_url_names, api, _)
+    def extract_events(group_url_names, api, week)
       ids = group_url_names.map { |name| api.get_meetup_id name }
 
-      api.get_upcoming_events(ids)
+      api.get_upcoming_events(ids, week)
     end
   end
 end
--- spec/lib/meetup_thingy/event_finder_spec.rb
+++ spec/lib/meetup_thingy/event_finder_spec.rb
@@ -13,14 +13,17 @@ describe MeetupThingy::EventFinder do
     end
 
     let(:events) { [:first_event, :another_event, :more_events, :final_event] }
+    let(:events_in_week) { [:event_in_week] }
 
     let(:api) { instance_double(MeetupThingy::MeetupAPI) }
 
     before do
       allow(api).to receive(:get_meetup_id) { |name| meetups[name] }
-      allow(api).to receive(:get_upcoming_events).with(meetups.values) { events }
+      allow(api).to receive(:get_upcoming_events).with(meetups.values, nil) { events }
+      allow(api).to receive(:get_upcoming_events).with(meetups.values, true) { events_in_week }
     end
 
     it { expect(subject.extract_events(meetups.keys, api, nil)).to eq(events) }
+    it { expect(subject.extract_events(meetups.keys, api, true)).to eq(events_in_week) }
   end
 end
