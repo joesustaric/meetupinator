abe3a89f6eaa49b70fca2c7d30886590abd3bd83
Joe Sustaric <jsustari@thoughtworks.com>
2015-02-15 19:48:15 +1100

    whoops forgot to add cli class

---
+++ lib/meetupanator/cli.rb
@@ -0,0 +1,39 @@
+require 'thor'
+require 'meetupanator'
+
+module Meetupanator
+  # class doco
+  class CLI < Thor
+    attr_accessor :event_finder
+    attr_accessor :event_list_file_writer
+    attr_accessor :api
+
+    class_option :meetup_api_key,
+                 type: :string, aliases: '-k',
+                 desc: 'API key for the meetup.com API,
+                        defaults to MEETUP_API_KEY environment
+                        variable if not set'
+
+    desc 'getevents', 'Write all upcoming events for the given meetup
+                       groups specified in INPUT to OUTPUT'
+    map 'getevents' => 'run_app'
+
+    option :input, aliases: '-i', required: true, type: :string
+    option :output, aliases: '-o', required: true, type: :string
+    option :week, aliases: '-w', required: false, type: :boolean
+
+    def run_app
+      Meetupanator::App.run(options)
+      puts "Output written to #{options[:output]}"
+    end
+
+    desc '--version', 'Print version'
+    map '--version' => 'version'
+    map '-v' => 'version'
+    def version
+      puts Meetupanator::App.version
+    end
+
+    default_task :getevents
+  end
+end
